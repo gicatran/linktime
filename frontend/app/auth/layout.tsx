0f95d3eeed7be6f392ad843c11193c9c222f55ba
Trần Lân Giác <tranlangiac@gmail.com>
2025-01-10 23:32:50 +0700

    Quick fix: Update auth's layout responsiveness

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -5,9 +5,9 @@ import Logo from "@/components/shared/Logo";
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
 	return (
-		<div className="flex h-screen">
-			<div className="w-1/2 flex justify-center items-center">
-				<div className="flex flex-col items-center">
+		<div className="flex h-screen relative">
+			<div className="w-full flex justify-center items-center xl:w-1/2 ">
+				<div className="flex flex-col items-center border rounded-lg px-10 py-20 xl:border-none">
 					<Logo className="h-14 w-auto" />
 					{children}
 				</div>
@@ -15,7 +15,7 @@ const AuthLayout = ({ children }: PropsWithChildren) => {
 			<Image
 				src={authBg}
 				alt="background"
-				className="w-1/2 object-cover"
+				className="w-1/2 object-cover hidden xl:block"
 			/>
 		</div>
 	);
